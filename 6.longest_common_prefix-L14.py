21f22b61697ee65c8c40dbb50a4e8950b47eb7c2
Jerin Joy <jerinjoy004@gmail.com>
2025-03-27 09:28:56 +0530

    5-6

---
+++ 6.longest_common_prefix-L14.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_length = float('inf')
+
+        for s in str:
+            if len(s) < min_length:
+                min_length = len(s)
+
+        i = 0
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+
+            i += 1
+
+        return s[:i]
+
+# time: O(n * m)
+# space: O(1)
